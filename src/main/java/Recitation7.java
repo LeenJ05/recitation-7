1e74c6a0439b967ff29c87529cfbe7de0255f100
Leen <leena@Leens-MacBook-Air.local>
2024-04-29 01:14:01 -0400

    Completed

--- src/main/java/Recitation7.java
+++ src/main/java/Recitation7.java
@@ -4,10 +4,37 @@ public class Recitation7 {
      * but you may NOT change the signatures of the methods below.
      */
     public static boolean lastItemReachable(int[] hopList){
-        return false; // replace this line with your code
+       int currentIndex = 0;
+       int targetIndex = hopList.length -1;
+
+       while (currentIndex < targetIndex){
+           int hop = hopList[currentIndex];
+           if (hop == 0){
+               return false;
+           }
+           currentIndex += hop;
+       }
+       return currentIndex == targetIndex;
+
     }
 
     public static int[] slidingWindowMin(int[] numberList, int windowSize){
-        return new int[0]; // replace this line with your code
+        if (windowSize <= 0 || windowSize > numberList.length) {
+            return new int[0];
+        }
+
+        int[] result = new int[numberList.length - windowSize + 1];
+
+        for (int i = 0; i <= numberList.length - windowSize; i++) {
+            int min = numberList[i];
+            for (int j = i + 1; j < i + windowSize; j++) {
+                if (numberList[j] < min) {
+                    min = numberList[j];
+                }
+            }
+            result[i] = min;
+        }
+
+        return result;
     }
 }
